8563e8e5c4d68afff092c68308ccccb9a5517672
B2110046 <honglam1115@gmail.com>
2024-04-15 14:17:58 +0700

    update project

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,77 +1,45 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{asset('/css/login.css')}}" rel="stylesheet"></link>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <title>Đăng nhập</title>
-    </head>
-    <body class="d-flex flex-column min-vh-100 gap-3 position-relative">
-        <header class="d-flex header">
-            <article class="d-flex gap-5">
-                <div class="logo">
-                    <!-- large-screen -->
-                    <a href="index.html">
-                        <img src="../images/logo.png" alt="logo">
-                    </a>
-                    <a href="index.html">E-read</a>
-                </div>
-                <nav class="nav">
-                    <!-- large-screen -->
-                    <a href="index.html">Sách</a>
-                    <a href="login.html">Box-chat</a>
-                    <div class="search-btn">
-                        <label for="search-btn"><img src="../images/search_logo.png" alt="Tìm"></label>
-                        <input type="search" name="search-btn" placeholder="Tìm kiếm">             
-                    </div>
-                </nav>
-            </article>
-            <article class="d-flex gap-3 right">
-                    <div>
-                        <a href="writing.html">Sáng tác</a>
-                    </div>    
-                    
-                    <div class="notification">
-                        <a href="#">Thư viện</a>
-                    </div>     
-                    <a href="login.html">Tài khoản</a>
-            </article>
-        </header>
+@extends('index-user')
 
-        <main class="pt-5 pb-5 container justify-content-center align-items-center">
-            <!-- position-absolute top-50 start-50 translate-middle -->
-            <article class="row d-flex justify-content-center align-items-center gap-5">
-                <div class="col-10 col-xl-6 d-flex justify-content-center">
-                    <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
-                        <!-- <div class="carousel-indicators">
+@section('title', 'Đăng nhập')
+
+@section('css')
+    <link rel="stylesheet" href=" {{ asset('css/login.css') }}">
+@endsection
+
+@section('main')
+    <main class="pt-5 pb-5 container justify-content-center align-items-center">
+        <!-- position-absolute top-50 start-50 translate-middle -->
+        <article class="row d-flex justify-content-center align-items-center gap-5">
+            <div class="col-10 col-xl-6 d-flex justify-content-center">
+                <div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+                    <!-- <div class="carousel-indicators">
                           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
                           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div> -->
-                        <div class="carousel-inner">
-                          <div class="carousel-item active">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
                             <img src="{{asset('images/image.png')}}" class="d-block w-80" alt="...">
-                          </div>
-                          <div class="carousel-item active">
-                            <img src="{{asset('images/image1.png')}}" class="d-block w-80" alt="...">
-                          </div>
-                          <div class="carousel-item active">
-                            <img src="{{asset('images/image.png')}}" class="d-block w-80" alt="...">
-                          </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                          <span class="visually-hidden">Next</span>
-                        </button>
-                      </div>
+                        <div class="carousel-item active">
+                            <img src="{{asset('images/image1.png')}}" class="d-block w-80" alt="...">
+                        </div>
+                        <div class="carousel-item active">
+                            <img src="{{asset('images/image.png')}}" class="d-block w-80" alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <form class="col-8 col-xl-3 p-3 container needs-validation login-form" novalidate>
+            </div>
+            <section class="container  col-8 col-xl-3 p-3 login-form">
+                <form class="needs-validation" novalidate>
                     <legend class="p-2">Đăng nhập</legend>
                     <div class="row justify-content-center pb-2">
                         <div class="col input-group">
@@ -84,58 +52,35 @@
                     </div>
                     <div class="row justify-content-center pb-2">
                         <div class="col input-group padding-custom-pass">
-                             <span class="input-group-text rounded-start-3" id="basic-addon1">Mật khẩu</span>
-                             <input type="password" class="form-control shadow-none rounded-end-3" placeholder="Nhập mật khẩu..." aria-label="Mật khẩu" aria-describedby="basic-addon1">
-                        </div> 
+                            <span class="input-group-text rounded-start-3" id="basic-addon1">Mật khẩu</span>
+                            <input type="password" class="form-control shadow-none rounded-end-3" placeholder="Nhập mật khẩu..." aria-label="Mật khẩu" aria-describedby="basic-addon1">
+                        </div>
                         <div class="invalid-feedback">
                             Sai mật khẩu.
                         </div>
-                     </div>
-                     <div class="row align-items-center">
+                    </div>
+                    <div class="row align-items-center">
                         <div class="col d-flex justify-content-center align-items-center">
                             <button class="btn-sign" type="submit">Đăng nhập</button>
                         </div>
                         <!-- btn btn-primary btn-sm -->
-                     </div>
-                     <hr>
-                     <div class="row">
-                        <div class="col d-flex flex-column justify-content-center align-items-center">
-                            <div class="forgot-pass-a">
-                                <a href="#">Quên mật khẩu?</a>
-                            </div>
-                            <div class="sign-up">
-                                <span>Chưa có tài khoản?</span>
-                                <a href="#">Đăng ký</a>
-                            </div>
-                        </div>
-                     </div>                    
-                </form>                
-            </article>
-        </main>
-
-        <footer class="container p-1 mt-auto">
-            <hr class="row hr">
-            <div class="row">
-                <nav class="d-flex flex-wrap justify-content-center justify-content-md-evenly align-items-center gap-2">
-                    <a href="#">Liên hệ</a>
-                    <a href="#">Về chúng tôi</a>
-                    <a href="#">Hợp tác</a>
-                    <a href="#">Điều khoản</a>
-                    <a href="#">Bảo mật</a>
-                    <a href="#">Thiết lập</a>
-                    <a href="#">Hỗ trợ</a>
-                    <a href="#">E-read</a>
-                </nav>
-            </div>
-            <div class="row pt-3">
-                <div class="col d-flex justify-content-center align-items-center">
-                    <div class="disc">
-                        Website được tạo ra với mục đích học tập và hoàn toàn phi lợi nhuận.<br>
-                        Tất cả các tác phẩm và một số chi tiết của website được tham khảo từ nhiều nguồn chưa được cho phép sử dụng bản quyền.
                     </div>
-            </div>
-        </footer>
-    
-    </body>
-    
-</html>
+                </form>
+                <hr>
+                <div class="row">
+                    <div class="col d-flex flex-column justify-content-center align-items-center">
+                        <div class="forgot-pass-a">
+                            <a href="#">Quên mật khẩu?</a>
+                        </div>
+                        <div class="sign-up">
+                            <span>Chưa có tài khoản?</span>
+                            <a href="#">Đăng ký</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+        </article>
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -10,40 +10,66 @@ use Illuminate\Support\Facades\Route;
 
 use function Ramsey\Uuid\v1;
 
-Route::get('/', [WorkListController::class, 'getNominations'])->name('home');
-
-Route::get('/test', function () {
-    return view('test');
-});
-
-// Route::get('/', function () {
-//     return view('works');
-// });
-
-Route::get('/tacpham', [WorkListController::class, 'index'])->name('works');
-
-Route::post('/tacpham/loc-theloai', [WorkListController::class, 'handleFilter'])->name('filter');
 
 Route::get('/dangnhap', function () {
     return view('login');   
 })->name('login');
 
-Route::get('/quantrivien/tacpham', [WorkManagementController::class, 'index'])->name('works.management');
 
-Route::get('/quantrivien/tacpham/chitiettacpham/{id}', [WorkManagementController::class, 'getWork'])->name('work.details');
+// Hiển thị trang chủ
+Route::get('/', [WorkListController::class, 'getNominations'])->name('home');
 
-Route::get('/quantrivien/tacpham/chitiettacpham/chinhsua/{id}', [WorkManagementController::class, 'edit'])->name('work.edit');
+// Xem danh sách tác phẩm
+Route::get('/tacpham', [WorkListController::class, 'index'])->name('works');
 
-Route::post('/quantrivien/tacpham/chitiettacpham/capnhat/{id}', [WorkManagementController::class, 'update'])->name('work.update');
+// Lọc tác phẩm theo thể loại
+Route::post('/tacpham/loc-theloai', [WorkListController::class, 'handleFilter'])->name('filter');
 
+// Đọc
+// Xem chi tiết thông tin tác phẩm
 Route::get('/tacpham/thongtintacpham/{id}', [WorkReadController::class, 'index'])->name('read.details');
 
+// Xem nội dung tác phẩm
 Route::get('/tacpham/doc/{id}', [WorkReadController::class, 'getContent'])->name('read.content');
 
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+//----------------------------------------------------------------
+
+// Quản lý tác phẩm
+// Hiển thị danh sách tác phẩm
+Route::get('/quantrivien/tacpham', [WorkManagementController::class, 'index'])->name('works.management');
+
+// Hiển thị chi tiết thông tin tác phẩm
+Route::get('/quantrivien/tacpham/chitiettacpham/{id}', [WorkManagementController::class, 'getWork'])->name('work.details');
+
+// Chỉnh sửa thông tin tác phẩm
+Route::get('/quantrivien/tacpham/chitiettacpham/chinhsua/{id}', [WorkManagementController::class, 'edit'])->name('work.edit');
+
+// Cập nhật các thay đổi của tác phẩm
+Route::post('/quantrivien/tacpham/chitiettacpham/capnhat/{id}', [WorkManagementController::class, 'update'])->name('work.update');
+
+
+// Quản lý tài khoản
+// Hiển thị danh sách tài khoản người dùng
 Route::get('/quantrivien/taikhoan/nguoidung', [AccountManagementController::class, 'index'])->name('accounts.management');
+
+// Hiển thị danh sách tài khoản người dùng đang hoạt động
 Route::get('/quantrivien/taikhoan/danghoatdong', [AccountManagementController::class, 'index'])->name('normal.account');
+
+// Hiển thị danh sách tài khoản người dùng đã bị khóa
 Route::get('/quantrivien/taikhoan/dakhoa', [AccountManagementController::class, 'index'])->name('block.account');
+
+// Hiển thị danh sách tài khoản biên tập viên
 Route::get('/quantrivien/taikhoan/bientapvien', [AccountManagementController::class, 'getEditorAccounts'])->name('editor.management');
+
+// Hiển thị danh sách tài khoản quản trị viên
 Route::get('/quantrivien/taikhoan/quantrivien', [AccountManagementController::class, 'getAdminAccounts'])->name('admin.management');
 
+
 // Route::get('/quantrivien/tacpham', [WorkListController::class, 'index'])->name('works');
+
+// Thử giao diện (không quan trọng)
+Route::get('/test', function () {
+    return view('test');
+});
